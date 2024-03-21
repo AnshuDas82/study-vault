004eb6a58a8d52472420e3dd6108b73836e2af55
Rahul Kumar <rahulkumar703@outlook.com>
2024-03-21 16:35:40 +0530

    added responsivness and some ui fixes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
 import bgImage from './Assets/bgvid.mp4';
 import Navbar from './Components/Navbar';
-import zeroone1 from './Assets/zeroone1.jpg';
+import zeroone1 from './Assets/zeroone1.png';
 import Sem from './Components/Sem';
 import Footer from './Components/Footer';
 
 
 function App() {
-  return (
-   <>  
-      <div className='logo'>
-      <img src={zeroone1} alt=''/>
-      </div>
-         <Navbar /> 
-         <div className="study">STUDY VAULT</div>
-      <div className="get">Get all the resources here at one place</div>
-     <video autoPlay loop muted playsInline>
-      <source src={bgImage} type="video/mp4"/>
-     </video>
-      <Sem/>
-      <Footer/>
-   </>
-  );
+   return (
+      <main className='main'>
+         <Navbar />
+         <div className="hero-section">
+            <div className='logo'>
+               <img src={zeroone1} alt='logo' />
+            </div>
+            <div className="overlay-text">
+               <div className="study">STUDY VAULT</div>
+               <div className="get">Get all the resources here at one place</div>
+            </div>
+            <video autoPlay loop muted playsInline>
+               <source src={bgImage} type="video/mp4" />
+            </video>
+         </div>
+         <Sem />
+         <Footer />
+      </main>
+   );
 }
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 import './Navbar.css';
+import zeroone1 from '../Assets/zeroone1.png';
 
 const Navbar = () => {
+
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+
+    const addActiveClass = () => {
+      const nav = document.querySelector('.nav');
+      if (window.scrollY > 300) {
+        nav.classList.add('active');
+        setActive(true);
+      }
+      else {
+        nav.classList.remove('active');
+        setActive(false);
+      }
+    };
+
+    document.addEventListener('scroll', addActiveClass);
+
+    return () => {
+      document.removeEventListener('scroll', addActiveClass);
+    }
+  }, [])
+
   return (
-<>
-    <div className='nav'>
-    <a href=''>Home</a>
-    <a href=''>Semester</a>
-    <a href=''>Contact</a>
-    </div>
-</>
-)};
+    <nav className='nav'>
+      {
+        active &&
+        <div className="logo">
+          <img src={zeroone1} alt="logo" />
+        </div>
+      }
+      <div className="links">
+        <a href='/'>Home</a>
+        <a href='/semester'>Semester</a>
+        <a href='/contact'>Contact</a>
+      </div>
+    </nav>
+  )
+};
 
 export default Navbar
